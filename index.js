e3739e32c370bad82cfccd638d8e4626ef85ae09
Angelfp29 <angelflores_2007Prado@hotmail.com>
2024-10-20 20:23:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabias que necesitabas a alguien para que te mande a dormir :p')
+    alert('Sabia que necesitabas a alguien para que te mande a dormir :p')
 });
 
 const noBtn = document.querySelector('#noBtn');
 
 noBtn.addEventListener('mouseover',function () {
-    const randomX = parseInt(Math.random()*400);
-    const randomY = parseInt(Math.random()*400);
+    const randomX = parseInt(Math.random()*300);
+    const randomY = parseInt(Math.random()*300);
     noBtn.style.setProperty('top',randomY+'%');
     noBtn.style.setProperty('left',randomX+'%');
     noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
